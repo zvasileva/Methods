e23168728fde8ab69c6a28f8e6271e621d252acf
zvasileva <zojka-homka@rambler.ru>
2020-06-15 01:27:45 +0300

    Готово!

--- src/js/__tests__/Daemon.test.js
+++ src/js/__tests__/Daemon.test.js
@@ -2,7 +2,7 @@ import Daemon from '../Daemon';
 
 test.each([
   ['Ivan', 'Daemon', {
-    name: 'Ivan', type: 'Daemon', health: 100, level: 2,  attack: 12, defence: 48,
+    name: 'Ivan', type: 'Daemon', health: 100, level: 2, attack: 12, defence: 48,
   }],
 ])('get result', (name, type, expected) => {
   const result = new Daemon(name, type);
